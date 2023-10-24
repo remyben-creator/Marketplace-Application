26ffe49b7c41d86a4bef79591b59f15bfbcc1ed7
bremy <bremy@vassar.edu>
2023-10-24 10:52:48 -0400

    fully working prototype(sell item, display all items, search) finished test-report with screenshots

--- Phase2/prototype/Controller.java
+++ Phase2/prototype/Controller.java
@@ -11,32 +11,6 @@ public class Controller {
         this.view = view;
     }
 
-    public void startApp() {
-        view.welcomeMessage();
-    }
-
-    public void runApp() {
-        boolean running = true;
-        while (running) {
-            view.displayMenu();
-            int choice = view.getUserChoice();
-
-            switch (choice) {
-                case 1:
-                    view.createPost();
-                    break;
-                case 2:
-                    view.displayPosts(model.getPosts());
-                    break;
-                case 3:
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
-
     public void createPost() {
         Post newPost = view.createPost();
         model.addPost(newPost);
@@ -46,4 +20,60 @@ public class Controller {
         List<Post> posts = model.getPosts();
         view.displayPosts(posts);
     }
+
+    public void searchPosts() {
+        String searchString = view.searchPosts().toUpperCase();
+        List<Post> posts = model.getPosts();
+        List<Post> matchingPosts = new ArrayList<>();
+
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+
+            String title = post.getTitle().toUpperCase();
+            String description = post.getDescription().toUpperCase();
+            String price = post.getPrice().toUpperCase();
+            String seller = post.getSeller().toUpperCase();
+
+            if (title.contains(searchString) || description.contains(searchString) ||
+                    price.contains(searchString) || seller.contains(searchString))
+                    {
+                matchingPosts.add(post);
+            }
+        }
+        if (matchingPosts.size() == 0) {
+            System.out.println("Sorry, no matches found.");
+        }
+        else { view.displayPosts(matchingPosts); }
+    }
+
+    public void startApp() {
+        view.welcomeMessage();
+    }
+
+    public void runApp() {
+        boolean running = true;
+        while (running) {
+            view.displayMenu();
+            String choice = view.getUserChoice();
+
+            switch (choice) {
+                case "1":
+                    createPost();
+                    break;
+                case "2":
+                    displayPosts();
+                    break;
+                case "3":
+                    searchPosts();
+                    break;
+                case "4":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+
 }
---
+++ Phase2/test-report.md
@@ -0,0 +1,34 @@
+# Test Report
+
+## Textual Description of expected functionality
+
+* When the application is started, there should be a welcome message upon booting
+then it will run.
+* While running, a menu will be shown with 4 options.
+  1. sell item (this will give prompt to fill out and list item based on inputs)
+  2. display available items (this will show all items in model's list)
+  3. search specific item (this will show all items with a matching user input keyword in model's list)
+  4. exit (this will stop the running of the program)
+* Option 1 will add an item to the list, and options 2 and 3 utilize that list. Therefore,
+if option 1 is chosen, then options 2 and 3 should reflect this.
+* Option 3 is not case-sensitive and searches for keyword within all fields. The tests 
+provided demonstrate both functionalities.
+
+
+## Proof of described functionality
+Choosing option 1:
+![IMG-2227.jpg](attachments%20%281%29%2FIMG-2227.jpg)
+Filling out info to sell item:
+![IMG-2228.jpg](attachments%20%281%29%2FIMG-2228.jpg)
+Choosing option 2 and seeing the item we just created:
+![IMG-2230.jpg](attachments%20%281%29%2FIMG-2230.jpg)
+Choosing option 3 and using a keyword to strike the title field:
+![IMG-2233.jpg](attachments%20%281%29%2FIMG-2233.jpg)
+Choosing option 3 and using a keyword to strike the description field:
+![IMG-2232.jpg](attachments%20%281%29%2FIMG-2232.jpg)
+Choosing option 3 and using a keyword to strike the price field:
+![IMG-2234.jpg](attachments%20%281%29%2FIMG-2234.jpg)
+Choosing option 3 and using a keyword to strike the seller field:
+![IMG-2235.jpg](attachments%20%281%29%2FIMG-2235.jpg)
+Choosing option 4 and having the application close:
+![IMG-2236.jpg](attachments%20%281%29%2FIMG-2236.jpg)
